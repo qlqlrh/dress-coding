0e94b2e86c8f5d80cdf563f61eba88ab3df10711
qlqlrh <dongin@pusan.ac.kr>
2025-04-03 02:24:15 +0900

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/example/dress/DressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
